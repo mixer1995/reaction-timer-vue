27eb30fc223f1f0d02233250513b67b5706a6793
Maksym Shkolnyi <mixer19995@gmail.com>
2024-05-07 12:07:23 +0300

    Finished app

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,34 @@
     <h1>Max's reaction trainer</h1>
     <button @click="start" :disabled="isPlaying">Begin</button>
     <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <Results v-if="scores && !isPlaying" :scores="scores" />
 </template>
 
 <script>
 import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 
 export default {
     name: 'App',
     components: {
-        Block
+        Block, Results
     },
     data() {
         return {
             isPlaying: false,
             delay: null,
-            score: null
+            scores: []
         }
     },
     methods: {
         start() {
             this.delay = 2000 + Math.random() * 5000;
             this.isPlaying = true;
+            this.score = null;
         },
         endGame(reactTime) {
-            console.log(reactTime);
+            this.scores.push(reactTime / 1000);
+            this.scores = this.scores.sort((a, b) => a - b).slice(0, 10);
             this.isPlaying = false;
         }
     }
@@ -40,5 +44,22 @@ export default {
     text-align: center;
     color: #444;
     margin-top: 60px;
+}
+
+button {
+    background: #0faf87;
+    color: white;
+    padding: 8px 16px;
+    border-radius: 4px;
+    border: none;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+}
+
+button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>Best scores:</h3>
+    <div v-for="(score, i) in scores">
+       {{ ++i }}) {{ score }} seconds
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        scores: {
+            type: Array
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
